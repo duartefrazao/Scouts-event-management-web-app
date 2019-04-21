131a9af6bad5cae5aec3c892d296d576d3688b6a
Margarida Silva <margarida.rpds@gmail.com>
2019-04-21 10:15:25 +0100

    [Add] styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,76 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!--<link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">-->
+    <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
+    
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Calendar Icon-->
+    <link rel="stylesheet" href="../css/calendar.css" type="text/css">
+
+    <!-- Member Icon-->
+    <link rel="stylesheet" href="../css/member.css" type="text/css">
+
+    <!-- Modal View-->
+    <link rel="stylesheet" href="../css/modal.css" type="text/css">
+
+    <!-- Event Page-->
+    <link rel="stylesheet" href="../css/event.css" type="text/css">
+
+    <!-- Log In -->
+    <link rel="stylesheet" href="../css/initial-page.css" type="text/css">
+
+    <!-- Profile -->
+    <link rel="stylesheet" href="../css/profile.css" type="text/css">
+
+    <!-- Log In -->
+    <link rel="stylesheet" href="../css/home-dash.css" type="text/css">
+
+    <!-- Groups listing -->
+    <link rel="stylesheet" href="../css/groups-listing.css" type="text/css">
+
+    <!-- Notifications -->
+    <link rel="stylesheet" href="../css/notification.css" type="text/css">
+
+    <!-- Admin -->
+    <link rel="stylesheet" href="../css/admin.css" type="text/css">
+
+    <!-- Faq -->
+    <link rel="stylesheet" href="../css/faq.css" type="text/css">
+
+    <!-- Create group -->
+    <link rel="stylesheet" href="../css/create-group.css" type="text/css">
+
+    <!-- Section Manage -->
+    <link rel="stylesheet" href="../css/section-manage.css" type="text/css">
+
+
+    <!-- Search -->
+    <link rel="stylesheet" href="../css/search.css" type="text/css">
+
+    <!-- About -->
+    <link rel="stylesheet" href="../css/about.css" type="text/css">
+
+    <!-- Common -->
+    <link rel="stylesheet" href="../css/common.css" type="text/css">
+
+    <!-- FontAwesome-->
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+        crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="../fontawesome/css/all.min.css" type="text/css">
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Acme|Francois+One|Indie+Flower|Josefin+Sans|Permanent+Marker|Quicksand|Sniglet|Rock+Salt"
+        rel="stylesheet">
+
+
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Nunito|PT+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Barrio|Kite+One|Questrial|Snippet" rel="stylesheet">
+
     <script type="text/javascript">
         // Fix for Firefox autofocus CSS bug
         // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
