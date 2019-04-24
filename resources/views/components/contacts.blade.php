bade4e294945c6a4d90f8d0fd06f38484bb3a979
duartefrazao <duartemarquesmano@gmail.com>
2019-04-24 03:03:16 +0100

    Small change to contacts

--- resources/views/components/contacts.blade.php
+++ resources/views/components/contacts.blade.php
@@ -2,21 +2,20 @@
     <div class="footer-left">
         <div>
             <i class="fa fa-map-marker"></i>
-            <span> Porto, Portugal </span>
+            <span> Viso, Viseu</span>
         </div>
         <div>
             <i class="fa fa-phone"></i>
-            <p>+1 555 123456</p>
+            <p>+232386765</p>
         </div>
         <div>
             <i class="fa fa-envelope"></i>
-            <p><a href="mailto:support@company.com">support@company.com</a></p>
+            <p><a href="mailto:geral.1351@escutismo.pt">geral.1351@escutismo.pt</a></p>
         </div>
     </div>
     <div class="footer-right">
         <p class="footer-company-about">
-            <span>About the company</span>
-            LBAW 2018.
+            <span>Sobre o agrupamento do Viso</span>
         </p>
     </div>
 </div>
